f87cdfd60aba2cc6a294b726cd5a68f1d49f6d2c
memo <alexis45645@gmail.com>
2023-02-05 20:35:40 -0500

    Initial commit

---
+++ src/Solution.java
@@ -0,0 +1,24 @@
+
+public class Solution {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(convert("PAYPALISHIRING", 3));
+	}
+	public static String convert(String s, int numRows) {
+		if (numRows == 1) return s;
+		StringBuilder[] rows = new StringBuilder[numRows];
+		for (int i = 0; i < numRows; i++)
+			rows[i] = new StringBuilder();
+		int curRow = 0;
+		boolean goingDown = false;
+		for (char c : s.toCharArray()) {
+			rows[curRow].append(c);
+			if (curRow == 0 || curRow == numRows - 1) goingDown = !goingDown;
+			curRow += goingDown ? 1 : -1;
+		}
+		StringBuilder ret = new StringBuilder();
+		for (StringBuilder row : rows) ret.append(row);
+		return ret.toString();
+	}
+}
